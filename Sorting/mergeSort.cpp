fad2760f68c7361f41668acfebed551ac23a34a2
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-04-15 01:46:30 +0530

    ,erge sort updated

--- Sorting/mergeSort.cpp
+++ Sorting/mergeSort.cpp
@@ -1,17 +1,7 @@
-/*
-    this is divide and conquer algorithm
-    which devides the array into two parts
-    and then merge them
-
-*/
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
-void merge(int arr[] , int low,int high,int mid){
-
-   
+void merge(int arr[],int low ,int mid ,int high){
     vector<int> temp;
     int left = low;
     int right = mid+1;
@@ -20,7 +10,6 @@ void merge(int arr[] , int low,int high,int mid){
         if(arr[left] <= arr[right]){
             temp.push_back(arr[left]);
             left++;
-
         }
         else{
             temp.push_back(arr[right]);
@@ -38,38 +27,34 @@ void merge(int arr[] , int low,int high,int mid){
         right++;
     }
 
-    for (int i = low; i <= high;i++){
+    for (int i = low; i <= high; i++)
+    {
         arr[i] = temp[i - low];
     }
-
-    
 }
- 
- void mergeSort(int arr[] , int low  ,int high){
-
     
-
-    if (low == high)
-    {
+ void mergeSort(int arr[], int low , int high){
+    if(low>= high){
         return;
-
     }
-     int mid = (low + high)/2;
-     mergeSort(arr, low, mid);
-     mergeSort(arr, mid+1, high);
-     merge(arr, low, mid, high);
-     
+
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid,high);
  }
- 
- 
+
 int main()
 {
-    int arr[10] = {1, 4, 2, 5, 6, 4, 6, 74, 5, 67};
-    mergeSort(arr, 0,10);
+    int arr[10] = {1, 4, 3, 5, 6, 9, 6, 4, 6, 5};
+    mergeSort(arr , 0 , 9);
 
     for (int i = 0; i < 10;i++){
-         cout << arr[i] << " ";
+        cout << arr[i] << " ";
+
     }
 
-         return 0;
+
+
+        return 0;
 }
