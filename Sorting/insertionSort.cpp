901223b58f637226b33bc79f38934e7698184895
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-06-23 00:36:02 +0530

    pankaj sir logic is added

--- Sorting/insertionSort.cpp
+++ Sorting/insertionSort.cpp
@@ -1,33 +1,45 @@
 /*
     takes the element and places its in its correct position
-
-
-
 */
 
 #include <bits/stdc++.h>
 using namespace std;
-    
- 
- void insertionSort(int arr[],int n){
-     for (int i = 0; i < n;i++){
-         int j = i;
-         while(j>0 && arr[j-1]>arr[j]){
-             swap(arr[j - 1], arr[j]);
-             j--;
-         }
 
-     }
- }
- 
- 
+void insertionSort(int arr[], int n)
+{
+    //  for (int i = 0; i < n;i++){
+    //      int j = i;
+    //      while(j>0 && arr[j-1]>arr[j]){
+    //          swap(arr[j - 1], arr[j]);
+    //          j--;
+    //      }
+        // Striver
+    //  }
+     
+    // pankaj sir code..
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(arr[i], arr[j]);
+
+
+            }
+        }
+    }
+}
+
 int main()
 {
-   int arr[6] = {4,7,15,3,10,8};
-   insertionSort(arr, 6);
-   for (int i = 0; i < 6;i++){
-       cout << arr[i] << " ";
-   }
+    int arr[5] = {50, 30, 20, 10, 40};
+    insertionSort(arr, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
-       return 0;
+    return 0;
 }
