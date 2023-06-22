4df4962eb7cbd43b12c1791ff3b287d9a060f959
Shivam singh <shivam.singhcs2020@indoreinstitute.com>
2023-06-23 02:51:32 +0530

    double pointer modified and mcq question added

--- pointer/doublePointer.cpp
+++ pointer/doublePointer.cpp
@@ -1,15 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
     
+//  douuble pointer 
+// when a pointer holds the address of 
+//another pointer is called double pointer
+
  
- 
- 
+void update(int **p){
+   //p = p + 1;
+   /*
+      no changes because function call is done by pass by value
+      no change in actual address of ptr2 
+   */
+
+ // *p = *p + 1;
+  /*
+      address of ptr will changed
+  */
+
+  **p = **p + 1;
+  /*
+      value present at i will achanged
+  */
+}
  
 int main()
 {
    
-   
-   
-   
+   int i = 5;
+   int *ptr = &i;                // pointer
+   int **ptr2 = &ptr;         //double pointer
+
+   // cout << i << endl;
+   // cout << *ptr << endl;
+   // cout << **ptr2 << endl;
+
+   /*
+      5
+      5
+      5
+   */
+
+   // cout << ptr << endl;
+   // cout << &i << endl;
+   // cout << *ptr2 << endl;
+
+   // cout << &ptr << endl;
+   // cout << ptr2 << endl;
+
+   /*
+      0x61feb8
+      0x61feb8
+      0x61feb8
+
+      0x61feb4
+      0x61feb4
+
+   */
+
+   cout << "before " << i << endl;
+   cout <<"before "<< ptr << endl;
+   cout << "before " <<ptr2<< endl;
+
+   update(ptr2);
+
+   cout << "after " << i << endl;
+   cout << "after " << ptr << endl;
+   cout << "after " << ptr2 << endl;
+
+
    return 0;
 }
---
+++ pointer/mcqOnPointer.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+    
+ 
+ 
+ 
+ 
+int main()
+{
+
+    // int first = 0;
+    // int second = 10;
+    // int *ptr = &second;
+    // *ptr = 9;
+
+    // cout << first << " " << second << endl;
+     // 0 9
+
+    // int first = 6;
+    // int *p = &first;
+    // int *q = p;                   7 ans
+    // (*q)++;
+    // cout << first << endl;
+
+    // int first = 8;
+    // int *p = &first;              8 9 ans
+    // cout << (*p)++ << " ";
+    // cout << first << endl;
+
+    // int *p = 0;
+    // int first = 110;
+    // *p = first;           it should be *p = &first 
+    //                       otherwise it will through seg fault        
+    // cout << *p << endl;
+
+    // int first = 8;
+    // int second = 11;
+    // int *third = &second;         11 13 ans
+    // first = *third;
+    // *third = *third + 2;
+    // cout << first << " " << second << endl;
+
+    // float f = 12.5;
+    // float p  = 21.5;
+    // float *ptr = &f;    // 21.5 21.5 21.5 ans
+    // (*ptr)++;
+    // *ptr = p;
+    // cout << *ptr << " " << f << " " << p << endl;
+
+    // int arr[5];
+    // int *ptr;          // 20 4 ans
+    // cout << sizeof(arr) << " " << sizeof(ptr) << endl;
+
+    
+
+    return 0;
+}
